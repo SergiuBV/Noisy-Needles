b3881997f5150e3214f984b5ca72e3469e1c376d
SergiuBV <126083311+SergiuBV@users.noreply.github.com>
2023-07-26 19:23:46 +0300

    changes 4

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-$(document).ready(function() {
-  $(".menu-icon").on("click", function() {
-        $("nav ul").toggleClass("showing");
-  });
-});
+let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
 
-// Scrolling Effect
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
 
-$(window).on("scroll", function() {
-  if($(window).scrollTop()) {
-        $('nav').addClass('black');
-  }
 
-  else {
-        $('nav').removeClass('black');
-  }
-})
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
+
 
 
--- js/about.js
+++ js/about.js
@@ -1,20 +1,28 @@
-$(document).ready(function() {
-    $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
-    });
-  });
-  
-  // Scrolling Effect
-  
-  $(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-          $('nav').addClass('black');
-    }
-  
-    else {
-          $('nav').removeClass('black');
-    }
-  })
+let menuIcon = document.querySelector('.menuIcon');
+        let nav = document.querySelector('.overlay-menu');
+
+        menuIcon.addEventListener('click', () => {
+            if (nav.style.transform != 'translateX(0%)') {
+                nav.style.transform = 'translateX(0%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            } else { 
+                nav.style.transform = 'translateX(-100%)';
+                nav.style.transition = 'transform 0.2s ease-out';
+            }
+        });
+
+
+        // Toggle Menu Icon ========================================
+        let toggleIcon = document.querySelector('.menuIcon');
+
+        toggleIcon.addEventListener('click', () => {
+            if (toggleIcon.className != 'menuIcon toggle') {
+                toggleIcon.className += ' toggle';
+            } else {
+                toggleIcon.className = 'menuIcon';
+            }
+        });
+
 
 
 
